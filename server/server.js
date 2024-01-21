eaf1beb6085f109774c884e881fc712f3b6f16a9
nthi2003 <quocthi.dev@gmail.com>
2024-01-21 23:43:51 +0700

    #8 s1

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 require('dotenv').config()
 import cors from 'cors'
 import initRoutes from './src/routes'
+import connectDatabase from './src/config/connectDatabase'
 
 const app = express()
 app.use(cors({
@@ -11,6 +12,7 @@ app.use(cors({
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 initRoutes(app)
+connectDatabase()
 app.use('/', (req,res) => {res.send('sever on ...')})
 
 const port = process.env.PORT || 8888
