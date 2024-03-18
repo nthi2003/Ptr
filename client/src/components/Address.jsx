5fc5cb98098c74d94afa5e805291ca2e381199c8
nthi2003 <quocthi.dev@gmail.com>
2024-03-18 22:58:23 +0700

    #60:

--- client/src/components/Address.jsx
+++ client/src/components/Address.jsx
@@ -1,16 +1,51 @@
-import React from 'react'
+import React , {useEffect, useState} from 'react'
 import { SelectAddress  } from '.'
+import { apiGetPublicProvinces, apiGetPublicDistrict } from '../services'
 
 const Address = () => {
+  const [provinces, setProvinces] = useState([])
+  const [districts, setDistricts] = useState([])
+  const [province, setProvince] = useState()
+  const [district, setDistrict] = useState()
+  const [reset, setReset] = useState(false)
+  useEffect(() => {
+     const fetchPublicProvinces = async() => {
+      const res = await apiGetPublicProvinces()
+      if(res.status === 200){
+        setProvinces(res?.data.results)
+      }
+     }
+     fetchPublicProvinces()
+  }, [])
+  useEffect(() => {
+    setDistrict(null)
+      const fetchPublicDistrict = async() => {
+        const res = await apiGetPublicDistrict(province)
+        if(res.status === 200){
+          setDistricts(res?.data?.results)
+        }
+       
+      }
+      province && fetchPublicDistrict(province)
+      !province ? setReset(true) : setReset(false)
+      !province && setDistrict([])
+  }, [province])
+
   return (
+
     <div>
       <h2 className='fon-semibold text-xl'>Địa chỉ cho thuê </h2>
       <div className='flex flex-col gap-4'>
-        <SelectAddress  label='Tỉnh/Thành phố'/>
-        <SelectAddress  label='Quận/Huyện'/>
+        <SelectAddress type='province' options={provinces} value={province} setValue={setProvince} label='Tỉnh/Thành phố'/>
+        <SelectAddress reset={reset} type='district' options={districts} value={province} setValue={setDistrict} label='Quận/Huyện'/>
       </div>
 
-       <input value='123' type="text" readOnly className='border border-gray-200 rounded-md outline-none bg-gray-100 p-2 w-full mt-10'/>
+       <div className='flex flex-col gap-2'>
+        <label className='font-medium' htmlFor="exactly-address">Địa chỉ chính xác</label>
+        <input id='exactly-address' type="text" readOnly className='border border-gray-200 rounded-md outline-none bg-gray-100 p-2 w-full'
+    value={`${district ? `${districts?.find(item => item.district_id === district)?.district_name},` : ''} ${province ? provinces?.find(item => item.province_id === province)?.province_name : ''}`}
+        />
+       </div>
   
     </div>
   )
--- client/src/services/app.js
+++ client/src/services/app.js
@@ -23,9 +23,22 @@ export const apiGetAreas = () => new Promise(async (resolve, reject) => {
        reject(error)
    }
 })
-export const apiGetProvinces = () => new Promise(async (resolve, reject) => {
+export const apiGetPublicProvinces = () => new Promise(async (resolve, reject) => {
     try {
         const response = await axios({
+            method: 'get',
+            url: 'https://vapi.vnappmob.com/api/province/'
+   
+        })
+       
+        resolve(response)
+    } catch (error) {
+        reject(error)
+    }
+ })
+ export const apiGetProvinces = () => new Promise(async (resolve, reject) => {
+    try {
+        const response = await axiosDefault({
             method: 'get',
             url: '/api/v1/province/all'
    
@@ -36,15 +49,14 @@ export const apiGetProvinces = () => new Promise(async (resolve, reject) => {
         reject(error)
     }
  })
- export const apiGetTest = () => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const response = await axios.get('/api/v1/test/all');
-            console.log(response);
-            resolve(response);
-        } catch (error) {
-            console.error('Error in apiGetTest:', error);
-            reject(error);
-        }
-    });
-};
+ export const apiGetPublicDistrict = (provinceId) => new Promise(async (resolve, reject) => {
+    try {
+        const response = await axiosDefault({
+            method: 'get',
+            url: `https://vapi.vnappmob.com/api/province/district/${provinceId}`
+        })
+        resolve(response)
+    } catch (error) {
+        reject(error)
+    }
+})
